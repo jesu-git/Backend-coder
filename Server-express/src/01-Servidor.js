1a032e75bae82d3c7bf646935cdeac3b6ce085f8
jesus suarez <suarezjesu90@gmail.com>
2023-11-01 15:05:17 -0300

    Modificando status y comprobaciones

--- Server-express/src/01-Servidor.js
+++ Server-express/src/01-Servidor.js
@@ -12,16 +12,16 @@ const products = pm.getProduct()
 
 //PAGINA PRINCIPAL
 app.get("/",(req,res)=>{
-  res.send("Servidor Express!!")
+  res.status(200).send("Servidor Express!!")
 })
 
 //MUESTRA LOS PRODUCTOS Y LIMITA POR PARAMETRO
 app.get("/products",(req,res)=>{
 
-  if(req.query.limit ==="")return res.send(products)
+  if(req.query.limit ==="")return res.status(200).send(products)
   else{
    let limitation =products.slice(0,req.query.limit)
-   res.send(limitation)
+   res.status(200).send(limitation)
   }
 })
 
@@ -29,9 +29,18 @@ app.get("/products",(req,res)=>{
 app.get("/products/:id",(req,res)=>{
   
  let id = req.params.id
- let serch = products.find((p)=> p.id == id)
+ 
+ if(id == NaN || id == "")return res.status(400).send("El id ingresado no es numerico")
+ 
+ else {
+      
+    let search = products.find((p)=> p.id == id)
+    
+      if(!search)return res.status(400).send("El id ingresado no existe")
+    
+      return res.status(200).send(search)}
 
- if(!serch) return res.send("El id ingresado es invalido")
- else{res.send(serch)}
+
+ 
  
 })
